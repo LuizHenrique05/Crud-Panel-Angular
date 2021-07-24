c88b120e9722a89a8d77f61d27bcf13ded7dd908
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2021-07-23 23:23:57 -0300

    first commit

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,39 @@
+import { Product } from './../product.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { 
+    this.product = {
+      name: '',
+      price: 0
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  deleteProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    if (id) {
+      this.productService.delete(id).subscribe(() => {
+        this.productService.showMessage('Product deleted!')
+        this.router.navigate(['/products'])
+      })
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
